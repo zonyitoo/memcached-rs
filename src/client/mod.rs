4f805214d2a3131b49a7d471f8696762665ff2c3
Jarred Nicholls <jtn@ironnetcybersecurity.com>
2015-08-07 14:12:06 -0400

    Make memcached-rs compatible with stable Rust.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -22,12 +22,13 @@
 //! Memcached client
 
 use std::net::TcpStream;
-use std::io::{self, BufStream};
+use std::io;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::ops::Deref;
 use std::path::Path;
 
+use bufstream::BufStream;
 use conhash::{ConsistentHash, Node};
 
 #[cfg(unix)]
@@ -51,12 +52,12 @@ impl Server {
                     match (split.next(), split.next()) {
                         (Some("tcp"), Some(addr)) => {
                             let stream = try!(TcpStream::connect(addr));
-                            box proto::BinaryProto::new(BufStream::new(stream)) as Box<Proto + Send>
+                            Box::new(proto::BinaryProto::new(BufStream::new(stream))) as Box<Proto + Send>
                         },
                         #[cfg(unix)]
                         (Some("unix"), Some(addr)) => {
                             let stream = try!(UnixStream::connect(&Path::new(addr)));
-                            box proto::BinaryProto::new(BufStream::new(stream)) as Box<Proto + Send>
+                            Box::new(proto::BinaryProto::new(BufStream::new(stream))) as Box<Proto + Send>
                         },
                         (Some(prot), _) => {
                             panic!("Unsupported protocol: {}", prot);
@@ -298,7 +299,7 @@ impl CasOperation for Client {
     }
 }
 
-#[cfg(test)]
+#[cfg(all(test, feature = "nightly"))]
 mod test {
     use test::Bencher;
     use client::Client;
