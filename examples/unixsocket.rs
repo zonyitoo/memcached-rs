04b4c890d60a4d82edb8f8840684346be9fe269e
Nick Amorim <nicholas.amorim@shopify.com>
2023-08-18 16:34:45 -0400

    Update examples to pass in an op_timeout option when connecting

--- examples/unixsocket.rs
+++ examples/unixsocket.rs
@@ -11,21 +11,15 @@ fn main() {
 
     let servers = [("unix:///tmp/memcached.sock", 10)];
     info!("Using servers: {:?} with Binary protocol", servers);
-    let mut client = Client::connect(&servers, ProtoType::Binary).unwrap();
+    let mut client = Client::connect(&servers, ProtoType::Binary, None).unwrap();
 
     client.set(b"Foo", b"Bar", 0xdead_beef, 2).unwrap();
     let (value, flags) = client.get(b"Foo").unwrap();
     assert_eq!(&value[..], b"Bar");
     assert_eq!(flags, 0xdead_beef);
 
-    client
-        .set_noreply(b"key:dontreply", b"1", 0x00_00_00_01, 20)
-        .unwrap();
+    client.set_noreply(b"key:dontreply", b"1", 0x00_00_00_01, 20).unwrap();
 
-    let (_, cas_val) = client
-        .increment_cas(b"key:numerical", 10, 1, 20, 0)
-        .unwrap();
-    client
-        .increment_cas(b"key:numerical", 1, 1, 20, cas_val)
-        .unwrap();
+    let (_, cas_val) = client.increment_cas(b"key:numerical", 10, 1, 20, 0).unwrap();
+    client.increment_cas(b"key:numerical", 1, 1, 20, cas_val).unwrap();
 }
